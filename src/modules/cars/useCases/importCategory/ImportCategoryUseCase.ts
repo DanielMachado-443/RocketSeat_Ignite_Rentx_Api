d90cfa2c0737d6fb6dd859a94c4187e4057f09ab
DanielMachado-443 <srandantte@gmail.com>
2022-05-04 18:46:27 -0300

    Chapter II, after 'Trabalhandocom Upload', working fine and clean

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,55 @@
+import { parse } from "csv-parse";
+import fs from "fs"; //<< It's Node's native module
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory{
+    name: string;
+    description: string;
+}
+
 class ImportCategoryUseCase {
-    execute(file: any){
-        console.log(file);
+    constructor(private categoriesRepository: ICategoriesRepository) {}  
+    
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject)  => {
+        const stream = fs.createReadStream(file.path);
+        const categories: IImportCategory[] = [];
+
+        const parseFile = parse();
+        
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async(line) => {
+            const [name, description] = line;
+            categories.push({
+                name, 
+                description,
+            });
+          })
+          .on("end", () => {        //<< Making the promising work
+              resolve(categories);
+          })
+          .on("error", (err) => {
+              reject(err);
+          })
+        });        
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {  
+        const categories = await this.loadCategories(file);           
+        
+        categories.map(async category => { //<< It iterates through categories array
+            const { name, description } = category;
+
+            const existCategory = this.categoriesRepository.findByName(name);
+            
+            if(!existCategory){
+                this.categoriesRepository.create({ 
+                   name, 
+                   description, 
+                });
+            }
+        })
     }
 }
 
