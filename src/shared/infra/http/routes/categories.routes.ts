9f7822c679b824521a42ae6e02e0680199c22b94
DanielMachado-443 <srandantte@gmail.com>
2022-06-07 15:17:24 -0300

    Working fine and clean, it's been capable of create cars verifying if whether or not the user is admin by the middleware on the main routes, as the category routes and cars routes, after 'Criando middleware de administrador', ok

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from "multer";
 import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController"
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
 
 const categoriesRoutes = Router();
 
@@ -15,13 +17,20 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.post("/", createCategoryController.handle); //<< By doing this way, the express package is able to pass the 'request, response' to the handle method
+categoriesRoutes.post(
+    "/",
+     ensureAuthenticated, //<< The order of middlewares call is important here. We need to check if we're logged in first
+     ensureAdmin,
+     createCategoryController.handle
+     ); //<< By doing this way, the express package is able to pass the 'request, response' to the handle method
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.post(
     "/import", 
     upload.single("file"),
+    ensureAuthenticated,
+    ensureAdmin,
     importCategoryController.handle
 ); 
 
