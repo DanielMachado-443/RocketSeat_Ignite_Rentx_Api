cf57630742f1a8748c5e84331415227fa45b06dd
DanielMachado-443 <srandantte@gmail.com>
2022-06-29 17:51:10 -0300

    It appears to be working so far, after 'Criando o primeiro teste de integracao', ok

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
+import { Router } from "express";
 import multer from "multer";
 
-import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController"
+import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
-import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
-import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const categoriesRoutes = Router();
 
 const upload = multer({
-    dest: "./tmp", 
+  dest: "./tmp",
 });
 
 const createCategoryController = new CreateCategoryController();
@@ -18,20 +18,20 @@ const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
 categoriesRoutes.post(
-    "/",
-     ensureAuthenticated, //<< The order of middlewares call is important here. We need to check if we're logged in first
-     ensureAdmin,
-     createCategoryController.handle
-     ); //<< By doing this way, the express package is able to pass the 'request, response' to the handle method
+  "/",
+  ensureAuthenticated, // << The order of middlewares call is important here. We need to check if we're logged in first
+  ensureAdmin,
+  createCategoryController.handle
+); // << By doing this way, the express package is able to pass the 'request, response' to the handle method
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
 categoriesRoutes.post(
-    "/import", 
-    upload.single("file"),
-    ensureAuthenticated,
-    ensureAdmin,
-    importCategoryController.handle
-); 
+  "/import",
+  upload.single("file"),
+  ensureAuthenticated,
+  ensureAdmin,
+  importCategoryController.handle
+);
 
 export { categoriesRoutes };
