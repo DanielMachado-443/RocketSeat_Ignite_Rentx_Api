f1d467d769df6dec28bb59d516bd6d9b36504cd7
DanielMachado-443 <srandantte@gmail.com>
2022-06-30 00:12:16 -0300

    It's apparently working and all the tests have passed, after 'Continuando teste de integracao', ok

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -5,43 +5,41 @@ import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/Us
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
-    sub: string;
+  sub: string;
 }
 
 export async function ensureAuthenticated(
-    request: Request, 
-    response: Response, 
-    next: NextFunction
-    ){
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
 
-    const authHeader = request.headers.authorization;
+  if (!authHeader) {
+    throw new AppError("Token missing", 401);
+  }
 
-    if(!authHeader){
-        throw new AppError("Token missing", 401);
+  const [, token] = authHeader.split(" "); // << Interesting strategy
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "cfe275a5908b5650488e0b0342c2d6cc"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError("User does not exists!", 401);
     }
 
-    const [, token] = authHeader.split(" "); // << Interesting strategy
+    request.user = {
+      id: user_id,
+    };
 
-    try{ 
-        const { sub: user_id } = verify(
-            token, 
-            "cfe275a5908b5650488e0b0342c2d6cc"
-            ) as IPayload;
-        
-        const usersRepository = new UsersRepository();
-        const user = await usersRepository.findById(user_id);
-
-        if(!user){
-            throw new AppError("User does not exists!", 401);
-        }        
-
-        request.user = {
-            id: user_id
-        }
-
-        next();
-    }
-    catch{
-        throw new AppError("Invalid token!", 401);
-    }
+    next();
+  } catch {
+    throw new AppError("Invalid token!", 401);
+  }
 }
