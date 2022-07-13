0b6116cc32c53a09753b681b5289c49932f4f611
DanielMachado-443 <srandantte@gmail.com>
2022-07-13 12:42:51 -0300

    Some tests that used the older token version has been fixed to match the new refresh_token title, some tests has been fixed and the last test has been added properly, after 'Refatorando testes', ok

--- src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUsecCase.spec.ts
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUsecCase.spec.ts
@@ -43,6 +43,24 @@ describe("List Cars", () => {
     expect(cars2).toEqual([car2]);
   });
 
+  it("should be able to list all available cars by name", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car3",
+      description: "Car description",
+      daily_rate: 110.0,
+      license_plate: "DEF-1235",
+      fine_amount: 40,
+      brand: "Car_brand_test",
+      category_id: "category_id",
+    });
+
+    const cars = await listAvailableCarsUseCase.execute({
+      name: "Car3",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+
   it("should be able to list all available cars by category", async () => {
     const car3 = await carsRepositoryInMemory.create({ // The 'create' also returns a car object
       name: "Car3",
